b82f11a4b5a9a22f0b4bd8d9df397a93c96b4edf
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-08-27 21:56:42 -0300

    exercicios de rapticoes

---
+++ condicionais/repetiçoes.js
@@ -0,0 +1,45 @@
+//console.log("FOR")
+//for (let contadora = 1; contadora <= 5; contadora++) {
+//    console.log(contadora)
+//}//variavel para contadora no mercado de trabalho é "i"
+
+
+//console.log("WHILE")
+//let contadorWHile = 1;
+//while (contadorWHile <= 5) {
+//    console.log(contadorWHile)
+//    contadorWHile++
+//}
+
+
+//EXERCICIO 1
+
+console.log("EXERCICIOS")
+
+//for(let cont = 1; cont<= 10; cont++){
+//console.log(cont)
+//}
+
+
+//EXERCICIO 2
+
+//let numero = Number(prompt("escolha um numero que te informarei uma tabuada"))
+//let i
+//let resultado = 0
+//for (i = 1; i <= 10; i++) {
+//    alert(resultado = numero * i)
+
+//}
+
+
+//EXERCICIO 3
+
+let Num = Number(prompt("digite qualquer numero"))
+let soma = 1
+while(soma <= Num){
+
+    alert(soma)
+     soma++
+     
+}  
+
