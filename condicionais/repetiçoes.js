726028cceefec567fbf35280e88077f043d6759c
PedroSt28 <stracierepedrohenrique@gmail.com>
2025-08-28 21:54:06 -0300

    praticando

--- condicionais/repetiçoes.js
+++ condicionais/repetiçoes.js
@@ -34,12 +34,83 @@ console.log("EXERCICIOS")
 
 //EXERCICIO 3
 
-let Num = Number(prompt("digite qualquer numero"))
-let soma = 1
-while(soma <= Num){
+//let Numb = Number(prompt("digite qualquer numero"))
+//let total = 0
+//i=1
 
-    alert(soma)
-     soma++
-     
-}  
+//while( i <= Numb){
+//     total= total+i
+//    alert(total)
+//     i++
+//}  
 
+
+//EXERCICIO INTERMEDIARIO 1
+
+
+//for (par = 1; par / 2 && par <= 50; par++) {
+//    if (par % 2 === 3 == 3) {
+//        alert(par)
+//    } else {
+//        par++
+//        alert(par)
+//    }
+
+//}
+
+
+//INTERMEDIARIO 2
+
+//let escolhido = Math.floor(Math.random(100 - 0) * 100)
+//let adivinhe = -1
+
+//while (escolhido != adivinhe) {
+//    adivinhe = Number(prompt("escolha o numero e eu falarei se esta perto ou nao"))
+//    if (escolhido > adivinhe) {
+//        alert(" menor que o numero ")
+//        console.log(escolhido)
+//    } else if (escolhido < adivinhe) {
+//        alert("maior que o numero")
+//    } else if (escolhido == adivinhe)
+//        alert("congratulations")
+//}
+
+
+//INTERMEDIARIO 3
+
+
+//let time = Number(prompt("contagem regressiva de : "))
+//while(time>0){
+//time--
+//alert(time)
+//}
+
+
+
+//DIFICIL 1
+
+
+//let num1 = String(prompt("digite um numero"))
+//let letrasList = num1.length
+//let i = 0
+//let soma = 0
+
+//while (i < letrasList) {
+//    soma += Number(num1[i]);
+//   alert(soma)
+//    i++
+//}
+
+
+//DIFICIL 2
+
+
+let number = Number(prompt("escolha um numero para saber o fatoreal"))
+let menor = number 
+let i  = number* (number-1)
+while(menor>=1){
+
+i * menor
+alert(i)
+
+}
